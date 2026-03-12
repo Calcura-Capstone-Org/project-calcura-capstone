af202c53a1f023b3b1d2db257951632074c1880f
ekwikings <ekwikingstad@liberty.edu>
2026-03-12 14:04:43 -0400

    testing/updating API endpoints

--- testapp.py
+++ testapp.py
@@ -1,3 +1,4 @@
+# Lines 1 - 22 written by Emma Wikingstad
 from fastapi import FastAPI
 from routers import users, roles, permissions, user_roles, budgets, sessions, categories, templates, template_items
 
@@ -11,3 +12,11 @@ app.include_router(sessions.router)
 app.include_router(categories.router)
 app.include_router(templates.router)
 app.include_router(template_items.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Calcura API"}
+
+# To run the app, use the command: uvicorn testapp:app --reload
+# If this command does not work, use "python -m uvicorn testapp:app --reload" instead
+# http://127.0.0.1:8000/docs#/ to access the API documentation and test the endpoints.
